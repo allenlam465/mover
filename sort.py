e083d243264c41f968b62fcad109f7ced831430f
Allen Lam <allenlam465@gmail.com>
2018-01-11 22:49:50 -0800

    Problem with browse buttons and apply. Function problems?

--- sort.py
+++ sort.py
@@ -2,20 +2,44 @@ import os
 import glob
 import shutil
 import errno
-import tkinter
+from tkinter import *
+from tkinter import filedialog
 
-def createFolder(cwd, fName):
-    try:
-        os.makedirs(cwd + "/" + fName)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            print("Folder exists.")
-            raise
+class Window(Frame):
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.__init__window()
 
-def getType(type, path):
-    for file in glob.glob(cwd + "/*." + type):
-        print(file)
-        shutil.move(file,path)
+    def __init__window(self):
+        self.master.title("Sorter")
+        self.pack(fill=BOTH, expand=1)
+        quitButton = Button(self,text="Quit", command=self.exit)
+        quitButton.place(x=0,y=0)
+
+    def exit(self):
+        exit()
+
+    def loadDir(self):
+        dirName = askopenfilename(filetypes)
+
+    def createFolder(cwd, fName):
+        try:
+            os.makedirs(cwd + "/" + fName)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                print("Folder exists.")
+                raise
+
+    def getType(type, path):
+        for file in glob.glob(cwd + "/*." + type):
+            print(file)
+            shutil.move(file,path)
+
+root = Tk()
+root.geometry("600x300")
+app = Window(root)
+root.mainloop()
 
 cwd = input("Input directory to be sorted: ")
 fName = input("Input folder name that files will be placed in: ")
